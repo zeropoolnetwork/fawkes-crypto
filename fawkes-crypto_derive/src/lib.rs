6978691afd23369278a46495d4aa3ec94aabd4c1
Dmitry Vdovin <voidxnull@gmail.com>
2021-05-01 22:37:20 +0200

    Add Signal macro implementation for enums

--- fawkes-crypto_derive/src/lib.rs
+++ fawkes-crypto_derive/src/lib.rs
@@ -2,7 +2,7 @@ extern crate proc_macro;
 use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{
-    parse_str, Data, DeriveInput, Field, Fields, FieldsNamed, FieldsUnnamed, Ident, Path, Type,
+    parse_str, Data, DeriveInput, Field, Fields, FieldsNamed, FieldsUnnamed, Ident, Path, Type, Variant
 };
 
 #[proc_macro_derive(Signal, attributes(Field, Value))]
@@ -75,6 +75,9 @@ fn expand(input: &DeriveInput, _: &str) -> TokenStream {
                 struct_impl(&field_vec, &field_path)
             }
             Fields::Unit => struct_impl(&[], &field_path),
+        },
+        Data::Enum(ref data_enum) => {
+            enum_impl(data_enum.variants.iter().cloned().collect(), &field_path)
         },
         _ => panic!("Only structs can derive a constructor"),
     };
@@ -200,5 +203,137 @@ fn struct_impl(fields: &[&Field], field_path:&Path) -> TokenStream {
         }
 
 
+    }
+}
+
+// TODO: Implement for tuple variants
+fn enum_impl(variants: Vec<Variant>, field_path: &Path) -> TokenStream {
+    let variant_names: Vec<Ident> = variants.iter().map(|v| v.ident.clone()).collect();
+
+    let field_names: Vec<Vec<Ident>> = variants
+        .iter()
+        .map(|v| {
+            if v.fields.is_empty() {
+                panic!("Variants cannot be empty");
+            }
+
+            v.fields
+                .iter()
+                .map(|f| f.ident.clone().unwrap())
+                .collect()
+        })
+        .collect();
+
+    // for destructuring the second argument
+    let other_field_names: Vec<Vec<Ident>> = field_names.iter().map(|fields| {
+        fields.iter().map(|field: &Ident| {
+            Ident::new(&format!("__other_{}", field), field.span())
+        }).collect::<Vec<Ident>>()
+    }).collect();
+
+    let first_field_names = field_names.iter().map(|names| names[0].clone());
+
+    quote! {
+        fn get_value(&self) -> Option<Self::Value> {
+            match self {
+                #(Self::#variant_names { #(#field_names),* } => {
+                    Some(Self::Value::#variant_names {#(#field_names: #field_names.get_value()?),*})
+                }),*
+            }
+        }
+
+        fn as_const(&self) -> Option<Self::Value> {
+            match self {
+                #(Self::#variant_names { #(#field_names),* } => {
+                    Some(Self::Value::#variant_names {#(#field_names: #field_names.as_const()?),*})
+                }),*
+            }
+        }
+
+        fn switch(&self, bit: &CBool<#field_path>, if_else: &Self) -> Self {
+            match self {
+                #(Self::#variant_names { #(#field_names),* } => {
+                    if let Self::#variant_names { #(#field_names: #other_field_names),* } = if_else {
+                        Self::#variant_names {#(#field_names: #field_names.switch(bit, #other_field_names)),*}
+                    } else {
+                        panic!("Signal::switch: variants do not match");
+                    }
+                }),*
+            }
+        }
+
+        fn get_cs(&self) -> &RCS<#field_path> {
+            match self {
+                #(Self::#variant_names { #(#field_names),* } => {
+                    #first_field_names.get_cs()
+                }),*
+            }
+        }
+
+        fn from_const(cs:&RCS<#field_path>, value: &Self::Value) -> Self {
+            match value {
+                #(Self::Value::#variant_names { #(#field_names),* } => {
+                    Self::#variant_names {#(#field_names: Signal::from_const(cs, #field_names)),*}
+                }),*
+            }
+        }
+
+        fn assert_const(&self, value: &Self::Value) {
+            match self {
+                #(Self::#variant_names { #(#field_names),* } => {
+                    if let Self::Value::#variant_names { #(#field_names: #other_field_names),* } = value {
+                        #(#field_names.assert_const(#other_field_names);)*
+                    } else {
+                        panic!("Signal::assert_const: variants do not match");
+                    }
+                }),*
+            }
+        }
+
+        fn inputize(&self) {
+            match self {
+                #(Self::#variant_names { #(#field_names),* } => {
+                    #(#field_names.inputize();)*
+                }),*
+            }
+        }
+
+        fn assert_eq(&self, other: &Self) {
+            match self {
+                #(Self::#variant_names { #(#field_names),* } => {
+                    if let Self::#variant_names { #(#field_names: #other_field_names),* } = other {
+                        #(#field_names.assert_eq(#other_field_names);)*
+                    } else {
+                        panic!("Signal::assert_eq: variants do not match");
+                    }
+                }),*
+            }
+        }
+
+        fn is_eq(&self, other: &Self) -> CBool<#field_path> {
+            let mut acc = self.derive_const(&true);
+
+            match self {
+                #(Self::#variant_names { #(#field_names),* } => {
+                    if let Self::#variant_names { #(#field_names: #other_field_names),* } = other {
+                        #(acc &= #field_names.is_eq(#other_field_names);)*
+                    }
+                }),*
+            }
+
+            acc
+        }
+
+        fn alloc(cs: &RCS<#field_path>, value: Option<&Self::Value>) -> Self {
+            if let Some(value) = value {
+                match value {
+                    #(Self::Value::#variant_names { #(#field_names),* } => {
+                        Self::#variant_names { #(#field_names: Signal::alloc(cs, Some(#field_names))),* }
+                    }),*
+                }
+            } else {
+                panic!("Signal::alloc on enums requires value to not be empty");
+            }
+        }
     }
 }
